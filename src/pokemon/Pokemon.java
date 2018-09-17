714bbd681fbc773dd6794c37f392dd037ed3ce02
Lucas Pellegrinelli <lucaspellegrinelli@hotmai.com>
2018-09-17 00:16:36 -0300

    Fixed identation

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -5,19 +5,19 @@ import types.Type;
 public class Pokemon {
   private int maxHP;
   private int baseAttack;
-	private int baseDefense;
-	private int baseSpAttack;
-	private int baseSpDefense;
-	private int baseSpeed;
-	
-	private Type type;
-	private MoveSet moveSet;
-	
-	public MoveSet getMoveSet() {
-		return this.moveSet;
-	}
-	
-	public Type getType() {
-		return this.type;
-	}
+  private int baseDefense;
+  private int baseSpAttack;
+  private int baseSpDefense;
+  private int baseSpeed;
+  
+  private Type type;
+  private MoveSet moveSet;
+  
+  public MoveSet getMoveSet() {
+    return this.moveSet;
+  }
+  
+  public Type getType() {
+    return this.type;
+  }
 }
